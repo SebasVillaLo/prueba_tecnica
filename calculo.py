e8b40d58b84a01b3f52dd10e2b73325906e952b2
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2022-10-11 12:10:28 -0500

    Initial commit

---
+++ calculo.py
@@ -0,0 +1,51 @@
+import tkinter
+
+
+def validador_entero(entero):
+    try:
+        if entero == '':
+            entero = 0
+        int(entero)
+        return int(entero)
+    except ValueError:
+        return tkinter.messagebox.showinfo("Error", "Revisa los campos donde pide un numero entero!")
+
+
+def calcular(nombre_empleado_entry, nombre_empresa_entry, nombre_contacto_cliente_entry, nombre_empresa_cliente_entry, costo_producto_entry, margen_estimado_entry):
+    # Creamos las variables para almacenar los datos ingresados
+    variables_principales = {'nombre_empleado': nombre_empleado_entry,
+                             'nombre_empresa': nombre_empresa_entry,
+                             'nombre_contacto_cliente': nombre_contacto_cliente_entry,
+                             'nombre_empresa_cliente': nombre_empresa_cliente_entry,
+                             'costo_producto': validador_entero(costo_producto_entry),
+                             'margen_estimado': validador_entero(margen_estimado_entry)}
+
+    mensajes_error = {
+        'nombre_empleado': "Nombre del empleado",
+        'nombre_empresa': "Nombre de la empresa",
+        'nombre_contacto_cliente': "Nombre del contacto del cliente",
+        'nombre_empresa_cliente': "Nombre de la empresa del cliente",
+        'costo_producto': "Costo del producto",
+        'margen_estimado': "Margen estimado"
+    }
+    inputs_vacios_list = []
+
+    for key, value in variables_principales.items():
+        if not value or value == "":
+            inputs_vacios_list.append(mensajes_error[key])
+
+    if len(inputs_vacios_list) > 0:
+        tkinter.messagebox.showinfo("Error", "No pudimos procesar el precio ya que no tenemos el {}!".format(
+            ', '.join(inputs_vacios_list)))
+    #  Resultado debe darle a precio de venta
+    else:
+        resultado = variables_principales['costo_producto'] / \
+            (1 - variables_principales['margen_estimado'] / 100)
+        tkinter.messagebox.showinfo("Resultado", '''
+    Hola {}!\n
+    El precio de venta del producto que deseas
+    entregar a {} de la
+    compañía {} es de ${:0,.2f}.\n
+    Con esta venta la {} tendrá
+    una utilidad de ${:0,.2f}.”
+        '''.format(variables_principales['nombre_empleado'], variables_principales['nombre_contacto_cliente'], variables_principales['nombre_empresa_cliente'], int(resultado), variables_principales['nombre_empresa'], int(resultado - variables_principales['costo_producto'])))
---
+++ main.py
@@ -0,0 +1,50 @@
+# Importamos la libreria tkinter para crear la interfaz grafica
+import tkinter
+import tkinter.messagebox
+from calculo import calcular
+
+# Creamos la ventana principal
+ventana = tkinter.Tk()
+ventana.title("Prueba Tecnica")
+ventana.geometry("500x400")
+imagen = tkinter.PhotoImage(file="assets/image/logo.png")
+min_resize_imagen = imagen.subsample(15, 15)
+logo = tkinter.Label(ventana, image=min_resize_imagen)
+logo.place(x=300, y=60)
+
+
+etiqueta = tkinter.Label(
+    ventana, text='Ingrese los datos solicitados para calcular el precio de venta del producto')
+etiqueta.place(x=10, y=10)
+etiqueta = tkinter.Label(ventana, text='Ingrese el nombre del empleado')
+etiqueta.place(x=10, y=60)
+nombre_empleado_entry = tkinter.Entry(ventana)
+nombre_empleado_entry.place(x=13, y=80)
+etiqueta = tkinter.Label(ventana, text='Ingrese el nombre de la empresa')
+etiqueta.place(x=10, y=110)
+nombre_empresa_entry = tkinter.Entry(ventana)
+nombre_empresa_entry.place(x=13, y=130)
+etiqueta = tkinter.Label(
+    ventana, text='Ingrese el nombre del contacto del cliente')
+etiqueta.place(x=10, y=160)
+nombre_contacto_cliente_entry = tkinter.Entry(ventana)
+nombre_contacto_cliente_entry.place(x=13, y=180)
+etiqueta = tkinter.Label(
+    ventana, text='Ingrese el nombre de la empresa del cliente')
+etiqueta.place(x=10, y=210)
+nombre_empresa_cliente_entry = tkinter.Entry(ventana)
+nombre_empresa_cliente_entry.place(x=13, y=230)
+etiqueta = tkinter.Label(ventana, text='Ingrese el costo del producto ($)')
+etiqueta.place(x=10, y=260)
+costo_producto_entry = tkinter.Entry(ventana)
+costo_producto_entry.place(x=13, y=280)
+etiqueta = tkinter.Label(ventana, text='Ingrese el margen estimado (%)')
+etiqueta.place(x=10, y=310)
+margen_estimado_entry = tkinter.Entry(ventana)
+margen_estimado_entry.place(x=13, y=330)
+boton = tkinter.Button(ventana, text='Calcular', command= lambda: calcular(nombre_empleado_entry.get(), nombre_empresa_entry.get(
+), nombre_contacto_cliente_entry.get(), nombre_empresa_cliente_entry.get(), costo_producto_entry.get(), margen_estimado_entry.get()))
+boton.place(x=340, y=130)
+
+
+ventana.mainloop()
